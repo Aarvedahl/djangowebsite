9277659ec808b6990a0d4da433902dba4650a723
Alex <alex.arvedahl@gmail.com>
2017-10-31 20:43:44 +0100

    Changed to Generic Views

--- music/urls.py
+++ music/urls.py
@@ -4,13 +4,7 @@ from .import views
 app_name = 'music'
 
 urlpatterns = [
-    #/music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 
-    #/music/(id)/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
-
-    
-    #/music/(id)/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name="favorite"),
 ]
