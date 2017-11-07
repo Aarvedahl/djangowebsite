12e2724537019e9ad0464169fc7a087fcc199f11
Alex <alex.arvedahl@gmail.com>
2017-11-07 10:46:34 +0100

    returning json objects

--- flask/hello.py
+++ flask/hello.py
@@ -1,6 +1,25 @@
 from flask import Flask
-app = Flask(__name__)
+from employee import Employee
+from flask import jsonify
 
+app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World! here is flask"
+
+@app.route("/tuesday")
+def tuesday():
+    return "It is tuesday today"
+
+@app.route("/json")
+def test():
+    employee = Employee('first name','last name')
+    return employee.toJson()
+
+@app.route("/list")
+def list():
+    return [1,2,3,4,5,6,7,8,9,10]
+
+#export FLASK_APP=hello.py python -m flask run
+
+
